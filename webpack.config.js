896a52af77ea326ed511dcab87a3a5462ce0c37a
Adrian Bobak <szuter8@gmail.com>
2019-01-17 09:31:23 +0100

    Install and setup Babel and Browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,14 @@ module.exports = {
     output: {
         path: resolve(__dirname + "/dist/"),
         filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
     }
 }
